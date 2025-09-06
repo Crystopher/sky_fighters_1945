96d61427eecff5ea01584750866a9865e40d03ec
Cristian Senzanonna <galford81@gmail.com>
2025-09-06 13:53:12 +0200

    added more features and intro for first level

---
+++ levels/level1/scenes/intro_01.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var animation_player = $intro01_1_player
+@onready var foot_steps_sound = $Footsteps
+@onready var engine_start = $EngineStart
+
+func _ready() -> void:
+	animation_player.play("intro01_1")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "intro01_1":
+		animation_player.play("intro01_2")
+		foot_steps_sound.play()
+	elif anim_name == "intro01_2":
+		animation_player.play("intro01_3")
+		engine_start.play()
+	elif anim_name == "intro01_3":
+		TransitionManager.change_scene("res://levels/level1/livello_1-1.tscn")
--- player_selection.gd
+++ player_selection.gd
@@ -11,7 +11,8 @@ func _on_button_2_button_down() -> void:
 
 func _on_button_2_pressed() -> void:
 	#get_tree().change_scene_to_file("res://levels/level1/livello_1-1.tscn")
-	TransitionManager.change_scene("res://levels/level1/livello_1-1.tscn")
+	#TransitionManager.change_scene("res://levels/level1/livello_1-1.tscn")
+	TransitionManager.change_scene("res://levels/level1/scenes/intro01.tscn")
 
 func _ready() -> void:
 	var fighters_selection = get_tree().get_nodes_in_group("player_selection")
